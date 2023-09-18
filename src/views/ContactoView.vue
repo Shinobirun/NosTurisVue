53f094e061a5fe24666057df0ffac670dcbadc56
shinobirun <adrian.latorre@gmail.com>
2023-09-18 07:46:56 -0300

    Agregados de Views

---
+++ src/views/ContactoView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <h1>{{ mensaje }}</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        mensaje: 'Componente Contacto'
+      };
+    }
+  };
+  </script>
+  
+  <style scoped>
+  /* Estilos específicos para este componente */
+  </style>
