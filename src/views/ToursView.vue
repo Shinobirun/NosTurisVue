42f6612517d3f2b89309fe0d3c4a95b8a291f20e
Marcela-Garcia <mgar616@gmail.com>
2023-09-20 19:17:15 -0300

    cambios

--- src/views/ToursView.vue
+++ src/views/ToursView.vue
@@ -1,46 +1,67 @@
 <template>
-    <div class="body">
-        <HeaderComponent />
-    <main>
-        <h1>{{ mensaje }}</h1>
-        <button @click="irAHome">Ir a Home</button>
-        <RelaxComponent></RelaxComponent> 
-        <RomanticComponent></RomanticComponent>
-    </main>
-    <FooterComponent />
-    </div>
-  </template>
+  <div class="body">
+      <HeaderComponent />
+  <main>
+
+<div class="filtro">
+    <label>
+      <input type="radio" v-model="filtro" value="todos" name="modoFiltro"> Todos
+    </label>
+    <label>
+      <input type="radio" v-model="filtro" value="relax" name="modoFiltro"> Relax
+    </label>
+    <label>
+      <input type="radio" v-model="filtro" value="romantico" name="modoFiltro"> Romántico
+    </label>
+</div>
+  <RelaxComponent v-if="filtro === 'relax'"></RelaxComponent> 
+  <RomanticComponent v-if="filtro === 'romantico'"></RomanticComponent>
+  <RelaxComponent v-if="filtro === 'todos'"></RelaxComponent>
+  <RomanticComponent v-if="filtro === 'todos'"></RomanticComponent>
+  </main>
   
-  <script>
-  
-  import HeaderComponent from '../components/HeaderComponent.vue';
+  <FooterComponent />
+  </div>
+</template>
+
+<script>
+
+import HeaderComponent from '../components/HeaderComponent.vue';
 import FooterComponent from '../components/FooterComponent.vue'; 
 import RelaxComponent from '../components/RelaxComponent.vue';
 import RomanticComponent from '../components/RomanticComponent.vue';
 
 export default {
-  name: 'ToursView',
-  components: {
-    HeaderComponent,
-    FooterComponent,
-    RelaxComponent,
-    RomanticComponent,
+name: 'ToursView',
+components: {
+  HeaderComponent,
+  FooterComponent,
+  RelaxComponent,
+  RomanticComponent,
+},
+data() {
+  return {
+    mensaje: 'Componente Turismo',
+    filtro: 'todos'
+  };
+},
+methods: {
+
+  filtrarRelax() {
+    this.filtro = 'relax';
   },
-  data() {
-    return {
-      mensaje: 'Componente Turismo'
-    };
+  filtrarRomantico() {
+    this.filtro = 'romantico';
   },
-  methods: {
-    irAHome() {
-      this.$router.push({ path: '/' });
-    }
+  mostrarTodos() {
+    this.filtro = 'todos';
   }
 }
-  </script>
-  
-  <style scoped>
-  .body {
+}
+</script>
+
+<style scoped>
+.body {
 
 font-family: 'Roboto', sans-serif;
 margin: 0;
@@ -49,5 +70,14 @@ background-image: url('../assets/images/playaMuelle.jpg');
 background-size: cover; /* Ajusta la imagen al tamaño del viewport */
 background-attachment: fixed; /* Fija la imagen de fondo para que no se desplace con el contenido */
 }
-  /* Estilos específicos para este componente */
-  </style>
+
+.filtro {
+display: flex;
+margin-bottom: 20px;
+}
+
+.filtro label {
+margin-right: 10px;
+}
+
+</style>
